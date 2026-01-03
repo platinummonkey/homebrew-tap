82d9b16e2f926083889d0aaf8faed7bc1779571e
Cody Lee <cody.lee@datadoghq.com>
2026-01-02 18:45:59 -0600

    something needs to be there

---
+++ Casks/legible.rb
@@ -0,0 +1,11 @@
+cask "legible" do
+  version "0.0.0" # Replace with the actual version
+  sha256 "calculated_shasum_from_step_1" # This should be pre-filled
+
+  url "[DOWNLOAD_URL]" # This should be pre-filled
+  name "Application Name" # Replace with the full application name
+  desc "A short description of the app" # Add a description
+  homepage "[APP_HOMEPAGE_URL]" # Add the application's homepage URL
+
+  # ... other stanzas will be generated based on the file type ...
+end
